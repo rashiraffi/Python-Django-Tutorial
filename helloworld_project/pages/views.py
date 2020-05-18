c7c9e4e0614ab62ebb939b157157e834ffccce22
rashiraffi <rashi1281@gmail.com>
2020-05-18 11:46:46 +0530

    Updated pages/views.py
    To add a function homepageview to return a Httpresponse('Hello world!').
    Imported Httpresponse from django.http

--- helloworld_project/pages/views.py
+++ helloworld_project/pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+"""
+Basically we’re saying whenever the view function homePageView is called, return
+the text “Hello, World!” More specifically, we’ve imported the built-in HttpResponse
+method so we can return a response object to the user. We’ve created a function called 
+homePageView that accepts the request object and returns a response with the string
+Hello, World! .
+"""
+
+
+def homePageView(request):
+    return HttpResponse('Hello, World')
