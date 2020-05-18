d05c8b1d938fac1e41b4f46a6aa21defe90c1252
rashiraffi <rashi1281@gmail.com>
2020-05-18 09:01:41 +0530

    Virtualenv files

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rashi/Documents/GIT_Repositories/Python-Django-Tutorial/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
