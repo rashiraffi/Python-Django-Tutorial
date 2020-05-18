cfd3ebc2524ba7eaeece9738d033fa0763c89a52
rashiraffi <rashi1281@gmail.com>
2020-05-18 11:36:54 +0530

    Added pages to installed apps

--- helloworld_project/helloworld_project/settings.py
+++ helloworld_project/helloworld_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.pagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
