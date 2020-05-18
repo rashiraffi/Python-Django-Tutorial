01fc8eb8ca3206e90adf6d62b64f0e08ca53b1e0
rashiraffi <rashi1281@gmail.com>
2020-05-18 11:59:45 +0530

    updated urls.py of project
    * imported include module from django.urls
    * added a new path to pades.urls

--- helloworld_project/helloworld_project/urls.py
+++ helloworld_project/helloworld_project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+"""imported include on the second line next to path and then created a new URLpattern for our pages app."""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
 ]
+"""Now whenever a user visits the homepage at / they will first be routed to the pages app and then to the homePageView view."""
