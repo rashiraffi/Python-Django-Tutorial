1ef6004df06f48cb5396197c0554a9db0134177f
rashiraffi <rashi1281@gmail.com>
2020-05-18 10:40:30 +0530

    Reset

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/rashi/Documents/GIT_Repositories/Python-Django-Tutorial/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
